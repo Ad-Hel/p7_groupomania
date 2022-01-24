cb583132cba5260e8eff2aa064464123c215cff2
Adrien <helary.adrien@gmail.com>
2022-01-24 14:26:25 +0100

    Feature picture.js Add "like" route

--- back/route/picture.js
+++ back/route/picture.js
@@ -8,5 +8,6 @@ router.put('/:id', pictureCtrl.modify);
 router.delete('/:id', pictureCtrl.delete);
 router.get('/:id', pictureCtrl.showOne);
 router.get('/', pictureCtrl.showAll);
+router.post('/:id/like', pictureCtrl.like);
 
 module.exports = router;
