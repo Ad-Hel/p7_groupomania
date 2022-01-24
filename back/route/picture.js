7a6ca20efd1c60f6bc8e011a9cd6b786501ceb19
Adrien <helary.adrien@gmail.com>
2022-01-24 11:23:58 +0100

    add picture.js (route)

---
+++ back/route/picture.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const pictureCtrl = require('../controller/picture');
+
+router.post('/', pictureCtrl.create);
+router.put('/:id', pictureCtrl.modify);
+router.delete('/:id', pictureCtrl.delete);
+router.get('/:id', pictureCtrl.showOne);
+router.get('/', pictureCtrl.showAll);
+
+module.exports = router;
