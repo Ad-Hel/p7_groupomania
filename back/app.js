d790610cc6867ecb879592704f39d2e5e6f4b8b6
Adrien <helary.adrien@gmail.com>
2022-01-24 11:22:21 +0100

    Add app.js

---
+++ back/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+
+const sequelize = require('./config/database.js');
+const app = express();
+
+
+app.use(express.json());
+
+const usersRoutes = require('./route/users.js');
+const picturesRoutes = require('./route/picture')
+
+async function connectionTest(){
+    try {
+        await sequelize.authenticate();
+        console.log('La connexion a été établie avec succès.');
+    } catch (error){
+        console.error('Impossible de se connecter à la base de données : ', error);
+    }
+}
+
+connectionTest();
+
+app.use('/api/auth', usersRoutes);
+app.use('/api/picture', picturesRoutes);
+
+module.exports = app;
