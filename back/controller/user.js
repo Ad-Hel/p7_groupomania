4ba4463aa6224874657efb01e55cb1bed9b9a299
Adrien <helary.adrien@gmail.com>
2022-01-24 17:25:56 +0100

    Feature user.js (controller) Add signin function

--- back/controller/user.js
+++ back/controller/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../model/User');
 
@@ -14,3 +15,28 @@ exports.signup = async (req, res, next) => {
         res.status(500).json({error});
     };   
 };
+
+exports.signin = async (req, res, next) => {
+    try{
+        const user = await User.findOne({where: { mail: req.body.mail}});
+        if (!user){
+            res.status(401).json({ error: "Utilisateur inconnu."})
+        };
+        console.log(user.id);
+        const pwd = await bcrypt.compare(req.body.password, user.password);
+        if (!pwd){
+            res.status(401).json({error: "Mot de passe inconnu."});
+        };
+        console.log("mot de passe : " + pwd)
+        res.status(201).json({
+            userId: user.id,
+            token: jwt.sign(
+                { userId: user.id },
+                'TOKEN_TO_HIDE_IN_ENV',
+                { expiresIn: '12h' }
+                )
+        });
+    } catch(error){
+        res.status(500).json(error);
+    }
+}
