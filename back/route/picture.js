ac7bc9f073e3bbf06b0022971d84d53f2935a348
Adrien <helary.adrien@gmail.com>
2022-01-26 20:05:48 +0100

    Update picture.js (route) Add the new middlewares

--- back/route/picture.js
+++ back/route/picture.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const ctrlPictureAuth = require('../middleware/ctrlPictureAuth');
+const multer = require('../middleware/multerConfig');
 
 const pictureCtrl = require('../controller/picture');
 
-router.post('/', auth, pictureCtrl.create);
-router.put('/:id', pictureCtrl.modify);
-router.delete('/:id', pictureCtrl.delete);
+router.post('/', auth, multer, pictureCtrl.create);
+router.put('/:id', ctrlPictureAuth, multer, pictureCtrl.modify);
+router.delete('/:id', ctrlPictureAuth, pictureCtrl.delete);
 router.get('/:id', pictureCtrl.showOne);
-router.get('/', pictureCtrl.showAll);
-router.post('/like/:id', pictureCtrl.like);
-router.delete('/like/:id', pictureCtrl.unlike);
+router.get('/', auth, pictureCtrl.showAll);
+router.post('/like/:id', auth, pictureCtrl.like);
+router.delete('/like/:id', auth, pictureCtrl.unlike);
 
 module.exports = router;
