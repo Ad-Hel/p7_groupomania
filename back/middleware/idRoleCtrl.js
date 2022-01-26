76e2eeba940b5cc3ff6bf1538e9aac30641770b8
Adrien <helary.adrien@gmail.com>
2022-01-26 16:37:09 +0100

    Fix idRoleCtrl.js remove a console.log

--- back/middleware/idRoleCtrl.js
+++ back/middleware/idRoleCtrl.js
@@ -1,8 +1,6 @@
 
 
 const idRoleCtrl = (userId, userRole, targetId, targetRole) => {
-    console.log(userRole);
-        console.log(targetRole);
     if (userId == targetId || userRole > targetRole){
         
         return true;
