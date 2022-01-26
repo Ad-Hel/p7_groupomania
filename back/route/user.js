26ba99e9d717be686d7cebfd8587df2444bf0d83
Adrien <helary.adrien@gmail.com>
2022-01-26 20:06:13 +0100

    Update user.js (route) Add the new middlewares

--- back/route/user.js
+++ back/route/user.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const authCtrl = require('../middleware/ctrlUserAuth');
+const ctrlUserRole = require('../middleware/ctrlUserRole');
+
 const userCtrl = require('../controller/user');
 
 router.post('/signup', userCtrl.signup );
 router.post('/signin', userCtrl.signin );
-router.get('/:id', userCtrl.showOne);
+router.get('/:id', authCtrl, userCtrl.showOne);
+router.put('/:id', authCtrl, ctrlUserRole, userCtrl.modifyOne);
 
 module.exports = router;
