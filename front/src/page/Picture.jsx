a27931a48ef19a0e0d21cb4d9472c005a1e4d26f
Adrien <helary.adrien@gmail.com>
2022-02-03 16:40:26 +0100

    Feature Modify Picture & User Add conditionnal display of a button and a form to modify

--- front/src/page/Picture.jsx
+++ front/src/page/Picture.jsx
@@ -2,15 +2,25 @@ import Header from "../layout/Header";
 import Container from "../layout/Container";
 import Footer from "../layout/Footer";
 import OnePicture from "../components/OnePicture";
+import FormPictureModify from "../components/FormPictureModify";
+
+import { useState } from 'react';
 
 
 function Picture(){
-    const id = new URLSearchParams(window.location.search).get('id');
+    const id = parseInt(new URLSearchParams(window.location.search).get('id'),10);
+    const auth = JSON.parse(window.localStorage.getItem('user'));
+    const [isModify, setIsModify] = useState(false);
     return(
         <div>
             <Header/>
             <Container>
+                {!isModify ?
                 <OnePicture id={id}/>
+                :
+                <FormPictureModify id={id}/>
+                }
+                {(auth.id === id && !isModify) && <button onClick={() => setIsModify(true)}>Modifier</button>}
             </Container>
             <Footer/>
         </div>
--- front/src/page/User.jsx
+++ front/src/page/User.jsx
@@ -2,19 +2,27 @@ import Header from "../layout/Header";
 import Container from "../layout/Container";
 import Footer from "../layout/Footer";
 import OneUser from "../components/OneUser";
+import { useState } from "react";
+import FormUserModify from "../components/FormUserModify";
 
 
 function User(){
-    const id = new URLSearchParams(window.location.search).get('id');
+    const id = parseInt(new URLSearchParams(window.location.search).get('id'),10);
+    const auth = JSON.parse(window.localStorage.getItem('user'));
+    const [isModify, setIsModify] = useState(false);
     return(
         <div>
             <Header/>
             <Container>
-                <OneUser id={id}/>
+                {isModify ?
+                <FormUserModify id={id}/> 
+                :
+                <OneUser id={id}/>}
+                {(auth.id === id && !isModify) && <button onClick={() => setIsModify(true)}>Modifier</button>}  
             </Container>
             <Footer/>
         </div>
-    )
+    ) 
 }
 
 export default User
