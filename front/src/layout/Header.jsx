bfb9122db5757c94e69a08ea9d0f0eb2db12363a
Adrien <helary.adrien@gmail.com>
2022-02-03 18:20:27 +0100

    Feature header Add signout button Add conditional display

--- front/src/layout/Header.jsx
+++ front/src/layout/Header.jsx
@@ -1,12 +1,26 @@
 import { Link } from 'react-router-dom';
+import isAuth from '../js/isAuth';
 
 function Header(){
+
+    function signout(){
+        window.localStorage.clear()
+        window.location.replace('http://localhost:3001/signin')
+    }
+
     return(
         <nav>
             <Link to="/">Home</Link> | {" "}
-            <Link to="/signup">Sign Up</Link> | {" "}
-            <Link to="/signin">Sign In</Link> | {" "}
+            { !isAuth() ?
+                <span>
+                    <Link to="/signup">Sign Up</Link> | 
+                    <Link to="/signin">Sign In</Link> | 
+                </span>
+                :
+                <button onClick={signout}>Sign Out</button>
+            }
             <Link to="/picture"> + </Link> | {" "}
+            
         </nav>
     );
 }
