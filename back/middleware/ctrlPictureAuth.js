e7c28d4bec8d1d2b3b83eef8e5f510f3dc4c08db
Adrien <helary.adrien@gmail.com>
2022-02-03 16:35:52 +0100

    Clean ctrlPictureAuth.js remove some "console.log" lines

--- back/middleware/ctrlPictureAuth.js
+++ back/middleware/ctrlPictureAuth.js
@@ -7,17 +7,12 @@ const Picture = require('../model/Picture');
 
 module.exports = async (req, res, next) => {
     try{
-        console.log("Middleware ok ")
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'TOKEN_TO_HIDE_IN_ENV');
         const userId = decodedToken.userId; 
-        console.log(userId);
         const userRole = decodedToken.userRole;
-        console.log(userRole);
         const picture = await Picture.findByPk(req.params.id);
-        console.log(picture);
         const target = await picture.getUser();
-        console.log(target);
         if (idRoleCtrl(userId, userRole, target.id, target.role)){
             next()
         } else{
