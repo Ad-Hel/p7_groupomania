9c0429ea03c1045ad81b831b37f8a09a42076a75
Adrien <helary.adrien@gmail.com>
2022-02-04 10:40:53 +0100

    Create like controller Add a new like.js controller Move like & dislike function Add isLiked and likesCount function

---
+++ back/controller/like.js
@@ -0,0 +1,42 @@
+const User = require('../model/User')
+const Picture = require('../model/Picture.js');
+
+exports.like = async (req, res, next) => {
+    try{
+        const picture = await Picture.findByPk(req.params.id);
+        const like = await picture.addUser(res.locals.userId);
+        res.status(200).json(like);
+    } catch(error){
+        res.status(404).json(error);
+    }
+}
+exports.unlike = async (req, res, next) => {
+    try{
+        const picture = await Picture.findByPk(req.params.id);
+        const unlike = await picture.removeUser(res.locals.userId);
+        res.status(200).json(unlike);
+    } catch(error){
+        res.status(404).json(error);
+    }
+}
+
+exports.likeCount = async (req, res, next) =>{
+    try{
+        const picture = await Picture.findByPk(req.params.id);
+        const likesCount = await picture.countUsers();
+        res.status(200).json(likesCount);
+    } catch(error){
+        res.status(404).json(error);
+    }
+}
+
+exports.isLiked = async (req, res, next) =>{
+    try{
+        const picture = await Picture.findByPk(req.params.id);
+        const isLiked = await picture.hasUser(res.locals.userId);
+        console.log(isLiked)
+        res.status(200).json(isLiked);
+    } catch{
+        res.status(404).json(error);
+    }
+}
--- back/controller/picture.js
+++ back/controller/picture.js
@@ -1,5 +1,5 @@
+const User = require('../model/User')
 const Picture = require('../model/Picture.js');
-const User = require('../model/User.js');
 const fs = require('fs/promises');
 const jwt = require('jsonwebtoken');
 
@@ -50,7 +50,9 @@ exports.delete = async (req, res, next) => {
 }
 exports.showAll = async (req, res, next) => {
     try{
-        const pictures = await Picture.findAll();
+        const pictures = await Picture.findAll({include: User});
+        const allPictures = [];
+        console.log(allPictures);
         res.status(200).json(pictures);
     } catch(error){
         res.status(400).json(error);
@@ -59,29 +61,7 @@ exports.showAll = async (req, res, next) => {
 exports.showOne = async (req, res, next) => {
     try{
         const picture = await Picture.findByPk(req.params.id);
-        res.status(200).json(picture);
-    } catch(error){
-        res.status(404).json(error);
-    }
-}
-exports.like = async (req, res, next) => {
-    try{
-        const user = await User.findByPk(req.body.UserId);
-        const picture = await Picture.findByPk(req.params.id);
-        const like = await picture.addUser(user);
-        const numberOfLike = await picture.countUsers();
-        res.status(200).json(numberOfLike);
-    } catch(error){
-        res.status(404).json(error);
-    }
-}
-exports.unlike = async (req, res, next) => {
-    try{
-        const user = await User.findByPk(req.body.UserId);
-        const picture = await Picture.findByPk(req.params.id);
-        const unlike = await picture.removeUser(user);
-        const numberOfLike = await picture.countUsers();
-        res.status(200).json(numberOfLike);
+        res.status(200).json(picture, isLiked, likeCount);
     } catch(error){
         res.status(404).json(error);
     }
