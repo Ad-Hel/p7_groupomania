04bab0435ca243e4ad936e2bbf22221dc9354cde
Adrien <helary.adrien@gmail.com>
2022-02-05 14:22:18 +0100

    Add global style

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.scss';
+import './scss/global.scss';
 import Home from './page/Home';
 import Signup from './page/Signup';
 import Signin from './page/Signin';
