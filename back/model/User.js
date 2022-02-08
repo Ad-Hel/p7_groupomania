1a7452d654c1780d79dc8274a8737384b4b712ff
Adrien <helary.adrien@gmail.com>
2022-02-08 14:35:37 +0100

    Fix User.js (model) Change type of role from string to integer

--- back/model/User.js
+++ back/model/User.js
@@ -24,7 +24,7 @@ User.init({
         unique: true
     },
     role: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
         defaultValue: 1
     },
