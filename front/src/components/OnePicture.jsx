913ce4570524d85249dcd7471999cfc22e2e6242
Adrien <helary.adrien@gmail.com>
2022-02-10 20:26:40 +0100

    Refactor OnePicture Add useAuth Remove useless try/catch Expand component logic

--- front/src/components/OnePicture.jsx
+++ front/src/components/OnePicture.jsx
@@ -2,47 +2,30 @@ import { useEffect, useState } from "react";
 import apiRequest from "../js/apiRequest";
 import LikeButton from "./LikeButton";
 import '../scss/component/picture.scss';
+import Picture from "./Picture";
+import useAuth from "./useAuth";
 
 function OnePicture(props){
-    const [picture, setPicture] = useState({
-        title: "Chargement",
-        imageUrl: "/",
-        id: 1
-    });
+    const [picture, setPicture] = useState(null);
     const id = props.id;
-    const auth = JSON.parse(window.localStorage.getItem('auth'))
+    const auth = useAuth().auth;
     useEffect( () => {
         async function getOnePicture(id) {
-            try{
-                const args = {
-                    token: auth.token,
-                    url: "picture/" + id,
-                }
-                const res = await apiRequest(args);
-                setPicture(res.data);
-                props.setUserId(res.data.UserId);
-            } catch(error){
-                console.log(error);
+            const args = {
+                token: auth.token,
+                url: "picture/" + id,
             }
+            const res = await apiRequest(args);
+            setPicture(res.data);
+            props.setUserId(res.data.UserId);
         }
         getOnePicture(id)
     }, [])
 
 
-    return(
-        <article className="picture">
-            <img className="picture__image" src={picture.imageUrl} alt="" />
-            <div className="picture__content">
-                <header className="picture__header">
-                    <h1 className="picture__title">{picture.title}</h1>
-                </header>
-                <footer className="picture__footer"> 
-                    <LikeButton auth={auth} picture={picture.id}/>
-                </footer>
-            </div>            
-        </article>
-        
-    )
+    return picture ? (
+        <Picture picture={picture} auth={auth}/>
+    ) : ( <p>chargement...</p>)
 }
 
 export default OnePicture
