2f933fd16153823edf73ca2bb168f45af9e3ea21
Adrien <helary.adrien@gmail.com>
2022-02-10 20:13:25 +0100

    Create Button component Delete SubmitButton component

--- front/src/components/ButtonSubmit.jsx
+++
@@ -1,8 +0,0 @@
-import '../scss/component/button-submit.scss'
-function ButtonSubmit(props){
-    return(
-        <button className="button-submit" type="submit">{props.label}</button>
-    )
-}
-
-export default ButtonSubmit
