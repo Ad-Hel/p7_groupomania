e86b2c6d2c9b0db530d7f9ebd831b706ea44c5ba
Adrien <helary.adrien@gmail.com>
2022-02-10 20:30:17 +0100

    Modify the title signup.jsx

--- front/src/page/Signup.jsx
+++ front/src/page/Signup.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Signup(){
     return(
         <Container>
-            <h1>Rejoindre la communauté Groupomania </h1>
+            <h1>S'inscrire</h1>
             <FormSignup/>
             <p>Si vous avez déjà un compte, c'est par <Link to="/signin">ici</Link>.</p>
         </Container>
