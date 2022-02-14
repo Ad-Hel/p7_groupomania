5c0230b0906abad5c3c547ee3e737349ed93e648
Adrien <helary.adrien@gmail.com>
2022-02-14 16:19:29 +0100

    Refactor Navigation.jsx add svg icons add close on exterior click for the dropdown remove signin and signup link

--- front/src/components/Navigation.jsx
+++ front/src/components/Navigation.jsx
@@ -4,7 +4,6 @@ import Button from './Button';
 import { useState } from 'react';
 import '../scss/component/nav.scss';
 
-
 function Navigation(){
     const Auth = useAuth();
     const auth = Auth.auth;
@@ -17,24 +16,20 @@ function Navigation(){
     }
     return(
         <nav className='nav'>
-            {
-                Auth.auth.id !== '' ?
-            <div>
-                <Link to="/">🏠</Link>  {"   "}
-                <Link to="/new"> ➕ </Link>  {"   "}
-                <Button type="button" classStyle="none" onclick={toggleDropdown}>menu</Button>
-                {isOpen && <div className='nav__dropdown'>
-                    <Link to={`/user/${auth.id}`}>{auth.firstName}</Link>
-                    <Button type="button" onclick={signout}>Sign Out</Button>
-                    </div>}
-                
-            </div>
-            :
-            <div>
-                <Link to="/signup">Sign Up</Link> {"   "} 
-                <Link to="/signin">Sign In</Link> {"   "}
-            </div>
-             }          
+                <Link to="/"><svg className="nav__icon" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5C575.8 273.5 560.8 287.6 543.8 287.6H511.8L512.5 447.7C512.5 450.5 512.3 453.1 512 455.8V472C512 494.1 494.1 512 472 512H456C454.9 512 453.8 511.1 452.7 511.9C451.3 511.1 449.9 512 448.5 512H392C369.9 512 352 494.1 352 472V384C352 366.3 337.7 352 320 352H256C238.3 352 224 366.3 224 384V472C224 494.1 206.1 512 184 512H128.1C126.6 512 125.1 511.9 123.6 511.8C122.4 511.9 121.2 512 120 512H104C81.91 512 64 494.1 64 472V360C64 359.1 64.03 358.1 64.09 357.2V287.6H32.05C14.02 287.6 0 273.5 0 255.5C0 246.5 3.004 238.5 10.01 231.5L266.4 8.016C273.4 1.002 281.4 0 288.4 0C295.4 0 303.4 2.004 309.5 7.014L564.8 231.5C572.8 238.5 576.9 246.5 575.8 255.5L575.8 255.5z"/></svg></Link>
+                <Link to="/new"><svg className="nav__icon" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM256 368C269.3 368 280 357.3 280 344V280H344C357.3 280 368 269.3 368 256C368 242.7 357.3 232 344 232H280V168C280 154.7 269.3 144 256 144C242.7 144 232 154.7 232 168V232H168C154.7 232 144 242.7 144 256C144 269.3 154.7 280 168 280H232V344C232 357.3 242.7 368 256 368z"/></svg></Link>
+                <div>
+                <Button type="button" classStyle="none" onclick={toggleDropdown}><svg className="nav__icon" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 112c-48.6 0-88 39.4-88 88C168 248.6 207.4 288 256 288s88-39.4 88-88C344 151.4 304.6 112 256 112zM256 240c-22.06 0-40-17.95-40-40C216 177.9 233.9 160 256 160s40 17.94 40 40C296 222.1 278.1 240 256 240zM256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-46.73 0-89.76-15.68-124.5-41.79C148.8 389 182.4 368 220.2 368h71.69c37.75 0 71.31 21.01 88.68 54.21C345.8 448.3 302.7 464 256 464zM416.2 388.5C389.2 346.3 343.2 320 291.8 320H220.2c-51.36 0-97.35 26.25-124.4 68.48C65.96 352.5 48 306.3 48 256c0-114.7 93.31-208 208-208s208 93.31 208 208C464 306.3 446 352.5 416.2 388.5z"/></svg></Button>
+                {isOpen && 
+                    <div className='nav__dropdown'>
+                        <div className='nav__dropdown__content'>
+                            <Link to={`/user/${auth.id}`}>Profil</Link>
+                            <Button type="button" classStyle='none' onclick={signout}>Déconnexion</Button>
+                        </div>
+                        <div className='nav__dropdown__background' onClick={toggleDropdown}></div>
+                    </div>
+                }        
+                </div>
         </nav>
     )
 }
