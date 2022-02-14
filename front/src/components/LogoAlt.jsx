a78e0199d86fb55bfaed72666d2e7ca3b0cd262e
Adrien <helary.adrien@gmail.com>
2022-02-14 16:22:08 +0100

    Create LogoAlt.jsx as alternate header

---
+++ front/src/components/LogoAlt.jsx
@@ -0,0 +1,9 @@
+import '../scss/component/logo-alt.scss'
+
+function LogoAlt(){
+    return(
+        <img src="/assets/logos/icon-left-font-monochrome-black.png" alt="" className='logo-alt'/>
+    )
+}
+
+export default LogoAlt
