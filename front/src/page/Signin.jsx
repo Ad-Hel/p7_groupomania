0441e029b1a542e38112e2f3d486920a6fe89576
Adrien <helary.adrien@gmail.com>
2022-02-14 16:26:30 +0100

    Style Signin.jsx and Signup.jsx Remove titles

--- front/src/page/Signin.jsx
+++ front/src/page/Signin.jsx
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom'
 function Signin(){
     return(
         <Container>
-            <h1>Se connecter</h1>
             <FormSignin />
             <p>Pas encore de compte ? C'est par <Link to="/signup">ici</Link>.</p>
         </Container>
--- front/src/page/Signup.jsx
+++ front/src/page/Signup.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 function Signup(){
     return(
         <Container>
-            <h1>S'inscrire</h1>
             <FormSignup/>
             <p>Si vous avez déjà un compte, c'est par <Link to="/signin">ici</Link>.</p>
         </Container>
