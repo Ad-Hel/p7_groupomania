d38494b498003e81ad93791891b86be93d87d2e7
Adrien <helary.adrien@gmail.com>
2022-02-15 12:57:06 +0100

    Refactor components logic 4 Merge OneUser and User.jsx

--- front/src/page/User.jsx
+++ front/src/page/User.jsx
@@ -1,22 +1,36 @@
-import Container from "../layout/Container";
-import OneUser from "../components/OneUser";
-import { useState } from "react";
-import FormUserModify from "../components/FormUserModify";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+
 import useAuth from "../components/useAuth";
 import apiRequest from "../js/apiRequest";
-import { useNavigate, useParams } from 'react-router-dom';
+
+import Container from "../layout/Container";
+import FormUserModify from "../components/FormUserModify";
 import Button from "../components/Button";
 
-
-
 function User(){
-    // const id = parseInt(new URLSearchParams(window.location.search).get('id'),10);
-    const { id } =  useParams();
-    const auth = useAuth().auth;
+    const [user, setUser] = useState(null);
     const [isModify, setIsModify] = useState(false);
     const [error, setError] = useState(null)
+
+    const auth = useAuth().auth;
     const navigate = useNavigate();
+
+    const { id } =  useParams();
     
+    useEffect( () => {
+        async function getUser(id){
+            const args = {
+                token: auth.token,
+                url: 'auth/' + id
+            }
+            const res = await apiRequest(args);
+            if ( res.status === 200 ){
+                setUser(res.data);            }
+        }
+        getUser(id);
+    }, [])
+
     async function handleDelete(){
         const args = {
             token: auth.token,
@@ -37,7 +51,7 @@ function User(){
         setIsModify(!isModify);
     }
 
-    return(
+    return user ? (
         <Container>
             {isModify ?
             <div>
@@ -46,10 +60,18 @@ function User(){
                 { error && <p>{error}</p>}
             </div>
             :
-            <OneUser id={id}/>}
+            <article className="user-card">
+                <h1 className="user-card__title">{user.firstName} {user.lastName}</h1>
+                <p className="user-card__text">Contacter par <a href={`mailto:${user.email}`}>email</a></p>
+            </article>
+            }
             {((auth.id === parseInt(id) || auth.role > 1 )&& !isModify) && <Button type='button' classStyle='edit' onclick={handleModify}>Modifier</Button>}
         </Container>
-    ) 
+    ) : (
+        <Container>
+            <p>Chargement...</p>
+        </Container>
+    )
 }
 
 export default User
