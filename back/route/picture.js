84d4a37474e56c19b854ec85f61f86c21b9696e1
Adrien <helary.adrien@gmail.com>
2022-02-17 17:01:10 +0100

    Fix picture.js Add a new route for deleted pictures Use the new middleware

--- back/route/picture.js
+++ back/route/picture.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-const ctrlPictureAuth = require('../middleware/ctrlPictureAuth');
+const isAuth = require('../middleware/isAuth');
+const isTargetValid = require('../middleware/isTargetValid');
 const multer = require('../middleware/multerConfig');
 
 const pictureCtrl = require('../controller/picture');
 
-router.post('/', auth, multer, pictureCtrl.create);
-router.put('/:id', ctrlPictureAuth, multer, pictureCtrl.modify);
-router.delete('/:id', ctrlPictureAuth, pictureCtrl.delete);
-router.get('/:id', auth, pictureCtrl.showOne);
-router.get('/', auth, pictureCtrl.showAll);
-router.get('/page/:page', auth, pictureCtrl.showAll);
-router.get('/mod/page/:page', auth, pictureCtrl.showAll);
-router.delete('/destroy/:id', pictureCtrl.destroyOne);
-router.put('/restore/:id', pictureCtrl.restoreOne);
+router.post('/', isAuth, multer, pictureCtrl.create);
+router.put('/:id', isAuth, isTargetValid, multer, pictureCtrl.modify);
+router.delete('/:id', isAuth, isTargetValid, pictureCtrl.delete);
+router.get('/:id', isAuth, pictureCtrl.showOne);
+router.get('/page/:page', isAuth, pictureCtrl.showAll);
+router.get('/deleted/page/:page', isAuth, pictureCtrl.showDeleted);
+router.delete('/destroy/:id', isAuth, isTargetValid, pictureCtrl.destroyOne);
+router.put('/restore/:id', isAuth, isTargetValid, pictureCtrl.restoreOne);
 
 module.exports = router;
