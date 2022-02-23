f066362311ec30841de5a51ff0ed175352251fca
Adrien <helary.adrien@gmail.com>
2022-02-23 11:14:39 +0100

    Add a props to use class style

--- front/src/components/Input.jsx
+++ front/src/components/Input.jsx
@@ -2,7 +2,7 @@ import '../scss/component/input.scss';
 function Input(props){
     return(
         <div className='input__group'>
-            <label htmlFor={props.name}>{props.label ? props.label : props.name}</label>
+            {props.label !== 'none' && <label htmlFor={props.name}>{props.label ? props.label : props.name}</label>}
             <input className={`input input--${props.type}`} type={props.type} id={props.name} name={props.name} value={props.value} onChange={props.onchange}/>
         </div>
     )
