00f2c1f8a2bf8b936784aa6113cddefb63f634de
Adrien <helary.adrien@gmail.com>
2022-03-02 17:33:03 +0100

    Comment conversation.jsx

--- front/src/components/Conversation.jsx
+++ front/src/components/Conversation.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 import useAuth from './useAuth';
 import apiRequest from '../js/apiRequest';
@@ -17,15 +17,18 @@ function Conversation(props){
     const [responses, setResponses] = useState(null);
     const [page, setPage] = useState(1);
     const [isLastPage, setIsLastPage] = useState(false);
-    const onLoad = useRef(true);
-
     
     useEffect(() => {
-        if ( onLoad.current ){
-            onLoad.current = false;
-            return;
-        }
-        
+       
+        /**
+         * 
+         * This function make an API call with a text id and page number to get its responses.
+         * 
+         * @name getResponses
+         * @function
+         * @param {integer} id 
+         * @param {integer} page 
+         */
         async function getResponses(id, page){
             const args = {
                 url: 'text/' + id + '/page/' + page,
@@ -39,15 +42,26 @@ function Conversation(props){
                 };
             };
         };
+
+        /**
+         * Avoid to get responses if it's hidden.
+         */
         if ( showResponses ){
             getResponses(text.id, page)
         }
-    }, [showResponses, page])
 
+    }, [showResponses, page, auth.token, text.id])
+
+    /**
+     * Toggle the display of responses.
+     */
     function handleShowResponses(){
         setShowResponses(!showResponses);
     }
 
+    /**
+     * Increment the page number.
+     */
     function handlePagination(){
         setPage(page + 1 );
     }
@@ -57,8 +71,8 @@ function Conversation(props){
             <div className="conversation__first-text">
                 <Text text={text} showResponses={showResponses} handle={handleShowResponses} list={props.texts} setList={props.setTexts}/>
             </div>
-            {(text.Texts && showResponses) && <div className="conversation__responses">
-                { responses && responses.map((response) => (
+            {showResponses && <div className="conversation__responses">
+                {responses && responses.map((response) => (
                     <Text key={response.id} text={response} list={responses} setList={setResponses} />
                 ))}
                 {!isLastPage && <Button type="button" classStyle="next" onclick={handlePagination}>Page suivante</Button>}
