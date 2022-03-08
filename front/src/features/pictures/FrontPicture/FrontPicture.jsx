0dca34782d6980ce57ae19c71926df9a9a909c13
Adrien <helary.adrien@gmail.com>
2022-03-08 17:43:01 +0100

    Fix front-picture Remove border in mobile to avoid overflow Use dark color for author to be WACG compliant

--- front/src/features/pictures/FrontPicture/FrontPicture.jsx
+++ front/src/features/pictures/FrontPicture/FrontPicture.jsx
@@ -17,12 +17,12 @@ function FrontPicture(props){
         <article className="front-picture">
                 <header className="front-picture__header">
                     <Link to={`/picture/${picture.id}`}>
-                        <h3 className="front-picture__title">{picture.title}</h3>
+                        <h2 className="front-picture__title">{picture.title}</h2>
                     </Link>                   
                 </header>
                 <img className="front-picture__image" src={picture.imageUrl} alt=""/>
                 <footer className="front-picture__footer">
-                    <Link to={`/user/${picture.UserId}`}>{picture.User.firstName} {picture.User.lastName}</Link>
+                    <Link to={`/user/${picture.UserId}`} className="front-picture__author">{picture.User.firstName} {picture.User.lastName}</Link>
                     {auth.role > 1 && <ModActions isMod list={props.items} setList={props.setItems} path="picture" id={picture.id} deletedAt={picture.deletedAt}/>}
                     <Reactions auth={auth} target={picture} path='picture' />
                 </footer>
