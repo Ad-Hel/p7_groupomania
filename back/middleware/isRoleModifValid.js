1f407a92410f9b1b1e785642148e93b5100740d0
Adrien <helary.adrien@gmail.com>
2022-03-14 12:03:22 +0100

    Fix isRoleModifValid Split the test to avoid blocking user modification

--- back/middleware/isRoleModifValid.js
+++ back/middleware/isRoleModifValid.js
@@ -10,9 +10,11 @@
  */
 module.exports = (req, res, next) => {
     try{
-        if (req.body.role && res.locals.userRole > req.body.role ){
+        if (!req.body.role){
             next();
-        }
+        } else if (res.locals.userRole > req.body.role){
+            next();
+        };
     }
     catch(error){
         res.status(403).json({message: "Vous n'êtes pas autorisé à réaliser cette action."})
